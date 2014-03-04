ba39a450f0673344c075e60f2254baff85990faf
Reinhard Simon <R.SIMON@CGIAR.ORG>
2014-03-04 10:05:33 -0500

    Fixed several bugs: - allele frequencies to be calculated by marker   (not by total) - keep missing markers across a set of genotypes (handle missing alleles for that marker) - added extra line for x-axis legend

--- R/quipu.R
+++ R/quipu.R
@@ -37,7 +37,7 @@ NULL
 NULL
 
 
-
+library(stringr)
 
 #library(pixmap)
 
@@ -76,7 +76,7 @@ rquipu <-  function (accession, marker, marker.size, map.location,
             ylim = c(50,350), 
             res=c(800,800),
             dir.print = "D:",
-            dir.logo = system.file("pictures/logo.ppm", package="pixmap")[1], 
+            dir.logo = NA, 
             col.fig = c("red3","gray30","gray40","gray50"), 
             col.marg = c("gray60","black","black"), 
             species.name = NA, 
@@ -114,30 +114,59 @@ rquipu <-  function (accession, marker, marker.size, map.location,
      }
    }
    datos=data.frame(datos)
-   dat=dat[order(datos[,5]),]
+   dat=dat[order(datos[,5], datos[,2],datos[,3]),]
    
-   c1=unique(dat$primer_name_original)
-   c2=unique(dat$Marker.size)
+#    c1=unique(dat$primer_name_original)
+#    c2=unique(dat$Marker.size)
    
    datt=data.frame(dat,peso=rep(0,nrow(dat)),color=rep(0,nrow(dat)))
-   tot=nrow(datt)
-   for(i in 1:length(c1))
-   {dt1=datt[datt[,2]==c1[i],]
-    if(nrow(dt1)!=0)
-    {for(j in 1:length(c2))
-    {dt2=dt1[dt1[,3]==c2[j],]
-     if(nrow(dt2)!=0)
-     {#print(dt2[,1:4])
-      posi=rownames(dt2)
-      ra=nrow(dt2) 
-      if(ra< (0.01*tot)){datt[posi,5]=rep(1.5,nrow(dt2));datt[posi,6]=rep(col.fig[1],nrow(dt2))}
-      if(ra>= (0.01*tot) & ra< (0.05*tot)){datt[posi,5]=rep(1.2,nrow(dt2));datt[posi,6]=rep(col.fig[2],nrow(dt2))}
-      if(ra>= (0.05*tot) & ra< (0.1*tot)){datt[posi,5]=rep(0.9,nrow(dt2));datt[posi,6]=rep(col.fig[3],nrow(dt2))}
-      if(ra > (0.1*tot)){datt[posi,5]=rep(0.6,nrow(dt2));datt[posi,6]=rep(col.fig[4],nrow(dt2))}
-     } 
-    }
-    }
-   } 
+#    tot=nrow(datt)
+#    for(i in 1:length(c1))
+#    {dt1=datt[datt[,2]==c1[i],]
+#     if(nrow(dt1)!=0)
+#     {for(j in 1:length(c2))
+#     {dt2=dt1[dt1[,3]==c2[j],]
+#      if(nrow(dt2)!=0)
+#      {#print(dt2[,1:4])
+#       posi=rownames(dt2)
+#       ra=nrow(dt2) 
+#       if(ra< (0.01*tot)){datt[posi,5]=rep(1.5,nrow(dt2));datt[posi,6]=rep(col.fig[1],nrow(dt2))}
+#       if(ra>= (0.01*tot) & ra< (0.05*tot)){datt[posi,5]=rep(1.2,nrow(dt2));datt[posi,6]=rep(col.fig[2],nrow(dt2))}
+#       if(ra>= (0.05*tot) & ra< (0.1*tot)){datt[posi,5]=rep(0.9,nrow(dt2));datt[posi,6]=rep(col.fig[3],nrow(dt2))}
+#       if(ra > (0.1*tot)){datt[posi,5]=rep(0.6,nrow(dt2));datt[posi,6]=rep(col.fig[4],nrow(dt2))}
+#      } 
+#     }
+#     }
+#    } 
+   
+   
+   # Calculate allele frequency by locus or primer pair
+   alls = paste(dat$primer_name ,dat$Marker.size,sep=".")
+   alls.fr=table(alls)
+   alls.to=table(dat$primer_name)
+   
+   up = unique(dat$primer_name)
+   for(a in 1:length(up) ){
+     pn = as.character(up[a])
+     alls.fr[str_detect(names(alls.fr),pn)]= 
+       alls.fr[str_detect(names(alls.fr),pn)]/alls.to[[pn]]
+   }
+
+   #print(str(alls.fr))
+   for(r in 1:nrow(datt)){
+     #print(str(datt))
+     an = paste(datt$primer_name_original[r],datt$Marker.size[r],sep=".")
+     #print(an)
+     ra = alls.fr[[an]]
+     
+     if(ra< (0.01))              {datt[r,5]=1.5; datt[r,6]=col.fig[1]}
+     if(ra>= (0.01) & ra< (0.05)){datt[r,5]=1.2; datt[r,6]=col.fig[2]}
+     if(ra>= (0.05) & ra< (0.1)) {datt[r,5]=0.9; datt[r,6]=col.fig[3]}
+     if(ra>= (0.1))              {datt[r,5]=0.6; datt[r,6]=col.fig[4]}
+     #print(paste(r, ra, datt[r,5],sep=" "))
+   }
+
+   
    
    moda=function(x)
    {
@@ -148,8 +177,13 @@ rquipu <-  function (accession, marker, marker.size, map.location,
    }
    
    ## Graphic
+   x = NA
+   if(!is.na(dir.logo)){
+     if(file.exists(dir.logo)){
+     x <- read.pnm(dir.logo) # reading the logo  
+   }
+   }
    
-   x <- read.pnm(dir.logo) # reading the logo
    
    clones=unique(datt$CIP.number)
    nameclones1=paste("CIP",unique(datt$CIP.number))
@@ -158,28 +192,33 @@ rquipu <-  function (accession, marker, marker.size, map.location,
    if(img.format=="jpeg") nameclones2=paste(dir.print,"/",nameclones1,".jpg", sep="")
    if(img.format=="png") nameclones2=file.path(dir.print,"/", paste(nameclones1,".png", sep=""))
    
+   mrcs=unique(datt$primer_name_original) 
+   
    for(j in 1:length(clones))
    {
      grup1=datt[datt$CIP.number==clones[j],]
-     mrcs=unique(grup1$primer_name_original) 
+     #mrcs=unique(grup1$primer_name_original) 
      
      ## print image 
      if(img.format=="jpeg") jpeg(nameclones2[j],quality = 100,width = res[1], height = res[2],pointsize = 22)
      if(img.format=="png") png(nameclones2[j],width = res[1], height = res[2],pointsize = 22)
+     
+     par(mar = c(6,4,4,2)+0.1)
      plot(1:length(mrcs),seq(min(grup1$Marker.size), max(grup1$Marker.size), length.out=length(mrcs)),
           type="n",axes=FALSE,ylab=list("Allele size [bp]",cex=ltr.size),
           #xlab=list("Chromosomes/SSR Name                                          ",cex=0.7, outer=TRUE),
           xlab="",
           main=c(paste(id.label,": ",nameclones[j], sep=""),""," "),
           cex.main=0.9,xlim=c(1,length(mrcs)+7),ylim=ylim)
-     mtext("                                                                                           Chromosomes/SSR name", cex=ltr.size, side=1, line=4, adj=0)
+     mtext("                                                                                      Chromosomes/SSR name", 
+           cex=ltr.size, side=1, line=5, adj=0)
      axis(2,seq(ylim[1],ylim[2],25),lwd=1.2,cex.axis=ltr.size,las=2, col=col.marg[2])  
      axis(3,at=1:length(mrcs),labels=1:length(mrcs),lwd=1.2,cex.axis=ltr.size, col=col.marg[3])
      axis(1, col = col.marg[1],at=1:length(mrcs) ,labels=mrcs,lty = 2, lwd = 1.2, cex.axis=ltr.size, las=2)
      
      
      ##abline(h = seq(ylim[1],ylim[2],25), v = 0, lty = 3, lwd = .1, col = "gray78")
-     
+     # horizontal lines
      for(i in seq(ylim[1],ylim[2],25))
      {lines(c(1,length(mrcs)),c(i,i),lty=3,lwd=0.8,col="gray80")
      }
@@ -194,18 +233,25 @@ rquipu <-  function (accession, marker, marker.size, map.location,
      
      cmp="inicio"
      ## printing circles 
+     mrcs = as.character(mrcs)
      for(i in 1:length(mrcs))
      {
        pt1=grup1[grup1$primer_name_original==mrcs[i],]
-       
-       if(pt1[1,4]==cmp){lines(c(i-1,i),c(ylim[2],ylim[2]),lty=1,lwd=2,col="gray90",type = "l")}
+       rom = as.character(datt[datt$primer_name_original == as.character(mrcs[i]),"Cromosomas"][1])
+       #print(pt1)
+       if(nrow(pt1)>0){
+        if(pt1[1,4]==cmp){
+          lines(c(i-1,i),c(ylim[2],ylim[2]),lty=1,lwd=2,col="gray90",type = "l")
+        }
        
        points(rep(i,nrow(pt1)),pt1[,3],pch=16,col=pt1[,6],cex=pt1[,5])
-       rom=moda(pt1[,4])[1]   
-       if(nchar(rom)>6){rom="unknw"}
-       text(i,(ylim[1]-5),as.factor(rom),cex=ltr.size)
-       
-       cmp = rom
+       if(is.null(rom)){#} | nchar(rom)>6 ) {
+         rom="unknw"
+       }
+      }
+      text(i,(ylim[1]-5),rom,cex=ltr.size)
+      
+      cmp = rom
      }
      
      
@@ -232,7 +278,10 @@ rquipu <-  function (accession, marker, marker.size, map.location,
      }
      legend(length(mrcs)+0.7,ylim[2]-60,imp,pch="",cex=ltr.size, title="Description") 
      
-     addlogo(x, px=c(length(mrcs)+0.7,length(mrcs)+6.5), py=c(70,125))
+     if(!is.na(x)){
+       addlogo(x, px=c(length(mrcs)+0.7,length(mrcs)+6.5), py=c(70,125))  
+     }
+     par(mar = c(5,4,4,2)+0.1)
      dev.off()
    }
    options(warn=1)
--- inst/examples/rquipu.R
+++ inst/examples/rquipu.R
@@ -11,7 +11,7 @@ dat = potato.quipu
 
 
 width = 1500
-height = 1000
+height = 1200
 
 rquipu(dat$accession_id, dat$primer_name, dat$marker_size, dat$map_location,
        species.name="Solanum tuberosum", set.name="Advanced clones",res=c(width,height),
