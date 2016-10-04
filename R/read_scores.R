cf6a6c7d27998f71eb28387f112ad8b82a39cba4
Frojast <rsimon64@gmail.com>
2016-10-04 16:49:51 -0500

    revise format checking

--- R/read_scores.R
+++ R/read_scores.R
@@ -39,16 +39,16 @@ read_scores <- function(file_name = NULL){
   # TODO consistency checks: all map names in score matrix, etc
   mp = marker2primer(names(scores)[-1])
   pr = unique(mp[, 1])
-  assert(all(pr %in% map$primer), "All marker names must have corresponding primer names in the map sheet.")
-  assert(all(!is.na(map$chromosome)), "The map file must not have a missing value for the chromosome.")
+  # assert(all(pr %in% map$primer), "All marker names must have corresponding primer names in the map sheet.")
+  # assert(all(!is.na(map$chromosome)), "The map file must not have a missing value for the chromosome.")
   
   pm = paste0(frq[, 1],".", frq[, 2])
-  assert(all(names(scores)[-1] %in% pm), 
-         paste("All marker names must have corresponding primer names in the frequencies sheet. Check:",
-               paste(pm[!names(scores)[-1] %in% pm]), collapse = ", ")) 
-  assert(all(!is.na(frq[, 3])), "The frequency table must not have any missing values. Check:")
-  assert(all(0 < frq[, 3] & frq[, 3] < 1), "The frequency values must be between 0 and 1.")
-  
+  # assert(all(names(scores)[-1] %in% pm), 
+  #        paste("All marker names must have corresponding primer names in the frequencies sheet. Check:",
+  #              paste(pm[!names(scores)[-1] %in% pm]), collapse = ", ")) 
+  # assert(all(!is.na(frq[, 3])), "The frequency table must not have any missing values. Check:")
+  # assert(all(0 < frq[, 3] & frq[, 3] < 1), "The frequency values must be between 0 and 1.")
+  # 
   out = scores
   })
 
